43321982f789b9db96dff6b8e480686d01edda11
Piotr Rutkowski <rutkowski1988@gmail.com>
2016-03-17 11:17:03 +0100

    Java 8 - Interfaces - default methods test

---
+++ java-playground/src/main/java/pl/prutkowski/java/playground/java8/TestDefaultInterfaces.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pl.prutkowski.java.playground.java8;
+
+import pl.prutkowski.java.playground.java8.domain.Boat;
+import pl.prutkowski.java.playground.java8.domain.Car;
+
+/**
+ *
+ * @author ruter
+ */
+public class TestDefaultInterfaces implements Car, Boat {
+
+	/**
+	 * @param args the command line arguments
+	 */
+	public static void main(String[] args) {
+		TestDefaultInterfaces defaultInterfaces = new TestDefaultInterfaces();
+		defaultInterfaces.fuel();
+	}
+
+	@Override
+	public void fuel() {
+		Car.super.fuel();
+		Boat.super.fuel();
+		System.out.println("Override fuel method runs.");
+	}	
+}
---
+++ java-playground/src/main/java/pl/prutkowski/java/playground/java8/domain/Boat.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pl.prutkowski.java.playground.java8.domain;
+
+/**
+ *
+ * @author ruter
+ */
+public interface Boat {
+	default void fuel() {
+		System.out.println(Boat.class.getSimpleName() + ".fuel() method runs.");
+	}
+}
